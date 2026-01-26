e59d1497be02a9daa686fc8a132c28feac09c4ff
gloretta <osadchuk.gloria@gmail.com>
2026-01-25 21:47:47 -0800

    Create Review -> Store.java

---
+++ Codecademy/Review -> Store.java
@@ -0,0 +1,17 @@
+public class Store {
+    // instance fields
+    public String productType;
+    public int inventoryCount;
+    public double inventoryPrice;
+
+    // constructor method
+    public Store(String product, int count, double price) {
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
+    }
+}
+
+
+
+// This is only the Store.java file! Look out for the Main.java file for this same exercise!
